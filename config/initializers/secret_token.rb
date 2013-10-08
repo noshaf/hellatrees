13f1d20d84cccef62492aefaa8370d84e8862703
Noah Shafi <noshaf@gmail.com>
2013-10-07 18:17:33 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hellatrees::Application.config.secret_token = '7014504efb789a553e5d861174a6056b99907e95e1903702a1eb395f8cbf125b888ec45c729f59120c989188a8400f12578467faa4a75cc0c0573a8e07ecfbe6'
